3b38252c67d1a0272efe2dde6711ded3f3a750e2
Stiliyan26 <stili262002@abv.bg>
2024-10-26 16:57:24 +0300

    Using generics template for travel_time

--- Exercise_2_Prefefiend_Frendly_Functions/examples/truck-friend-func-class-member.cpp
+++ Exercise_2_Prefefiend_Frendly_Functions/examples/truck-friend-func-class-member.cpp
@@ -3,83 +3,71 @@ using namespace std;
 
 // Forward declaration
 class Car;
-class Truck;
-
-template <typename T>
-int travel_time(const T &t);
 
 class Truck
 {
-private:
-    int weight;
-    int speed;
-    int distance;
-
 public:
-    Truck(int w, int s, int d)
+    Truck(int w, int s)
     {
         weight = w;
         speed = s;
-        distance = d;
     };
-
+    
     int sp_greater(Car c);
 
-    friend int travel_time<>(const Truck &t);
+private:
+    int weight;
+    int speed;
 };
 
 class Car
 {
-private:
     int passengers;
     int speed;
-    int distance;
 
 public:
-    Car(int p, int s, int d)
+    Car(int p, int s)
     {
         passengers = p;
         speed = s;
-        distance = d;
     };
 
     friend int Truck::sp_greater(Car c);
-
-    friend int travel_time<>(const Car &c);
 };
+
 
 int Truck::sp_greater(Car c)
 {
     return c.speed - speed;
 }
 
-template <typename T>
-int travel_time(const T &t)
-{
-    if (t.speed == 0)
-        return -1; 
-    return t.distance / t.speed; 
-}
 
 int main()
 {
-    Car c1(5, 100, 500); 
-    Truck t1(10000, 80, 500); 
+    int t;
 
-    int t = t1.sp_greater(c1);
+    Car c1(6, 55), c2(2, 120);
+    Truck t1(10000, 55), t2(20000, 72);
+
+    cout << "Comparing c1 and t1:\n";
+    t = t1.sp_greater(c1);
 
     if (t < 0)
         cout << "Truck is faster.\n";
     else if (t == 0)
-        cout << "Car and truck have the same speed.\n";
+        cout << "Car and truck speed is the same.\n";
     else
         cout << "Car is faster.\n";
 
-    int timeCar = travel_time(c1);
-    cout << "Travel time for the car: " << timeCar << " hours.\n";
+    cout << "\nComparing c2 and t2:\n";
+    t = t2.sp_greater(c2);
 
-    int timeTruck = travel_time(t1);
-    cout << "Travel time for the truck: " << timeTruck << " hours.\n";
+    if (t < 0)
+        cout << "Truck is faster.\n";
+    else if (t == 0)
+        cout << "Car and truck speed is the same.\n";
+    else
+        cout << "Car is faster.\n";
 
     return 0;
 }
